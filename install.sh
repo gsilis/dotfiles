105604ef905e739e1e77e4a69b7b3465420152d0
Girts Silis <girts.silis@gmail.com>
2015-10-05 23:12:41 -0500

    Install script for vim stuff

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check for things we depend on
+if command -v brew >/dev/null; then
+  echo "Checking for brew.. OK"
+else
+  echo "Run this after you install brew "; exit 1
+fi
+
+if command -v mvim >/dev/null; then
+  echo "Checking for macvim.. OK"
+else
+  echo "Installing mvim via brew..."
+  brew install macvim
+fi
+
+# Link stuff
+ln -s ./home/.vim ~/.vim
+ln -s ./home/.vimrc ~/.vimrc
+ln -s /usr/local/bin/mvim /usr/local/bin/vim
+
+# Make sure we have plugins
+vim +PluginInstall +qall
+
